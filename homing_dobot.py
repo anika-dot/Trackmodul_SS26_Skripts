05745b3da85fa96fc02f9425186bf6d367176014
anika-dot <heimani1@students.zhaw.ch>
2026-05-12 11:28:50 +0200

    Refactor dobot initialization and logging for clarity

--- homing_dobot.py
+++ homing_dobot.py
@@ -6,11 +6,14 @@ ports = find_dobot_ports()
 print(ports)
 
 # ✅ Richtige Ports wählen!
-dobot = init_and_home_dobot(ports[0])
+dobot_pickplace = init_and_home_dobot(ports[0])
+dobot_sorter = init_and_home_dobot(ports[1])
 
 time.sleep(1)
 
-print("dobot:", dobot)
+print("dobot pickplace:", dobot_pickplace)
+print("dobot sorter:", dobot_sorter)
 
 # ✅ ERST AM ENDE schließen
-dobot.interface.close_col()
+dobot_pickplace.interface.close_col()
+dobot_sorter.interface.close_col()
